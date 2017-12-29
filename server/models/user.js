d660da26ea892c3f75d7875823332b00c1e04c55
beth92 <bethtownsend92@gmail.com>
2017-12-29 01:03:07 +0000

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -99,6 +99,25 @@ UserSchema.statics.findByToken = function(token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  // start by matching email to get a potential match
+  let User = this;
+  return User.findOne({email}).then( (user) => {
+      if (!user) {
+        return Promise.reject();
+      }
+      return new Promise( (resolve, reject) => {
+        bcrypt.compare(password, user.password, (err, result) => {
+          if(result){
+            resolve(user);
+          } else {
+            reject();
+          }
+        });
+      });
+  });
+};
+
 // run some code before save events to hash pw before storing
 UserSchema.pre('save', function (next) {
   let user = this;
--- server/server.js
+++ server/server.js
@@ -149,6 +149,23 @@ app.get('/users/me', authenticate, (req, res) => {
   });
 });
 
+// POST /users/login {email, password}
+
+// whose hashed password compares to plaintext password using brypt compare
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password)
+  .then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.set('x-auth', token).send(user);
+    });
+  })
+  .catch( (e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 } );
